ee81fb04ef218459b25628aee1c00c97b439122f
peternara <peternara@users.noreply.github.com>
2022-03-13 22:37:46 +0900

    Update memory_bank.py

--- benchmark/utils/memory_bank.py
+++ benchmark/utils/memory_bank.py
@@ -24,14 +24,16 @@ class MemoryBank:
         index      = faiss.index_cpu_to_all_gpus(index)
         index.add(self.features)
         _, indices = index.search(features, topk + 1) # Sample itself is included
+        
         return indices[:, 1:] # 자기 자신을 제거
     
     def compute_nearest_neighbors_accuracy(self, topk):
         if isinstance(topk, int):
             topk = [topk]
-        topk = sorted(topk)
-        index = faiss.IndexFlatIP(self.feature_dim)
-        index = faiss.index_cpu_to_all_gpus(index)
+            
+        topk       = sorted(topk)
+        index      = faiss.IndexFlatIP(self.feature_dim)
+        index      = faiss.index_cpu_to_all_gpus(index)
         index.add(self.features)
         _, indices = index.search(self.features, topk[-1] + 1) # Sample itself is included
 
@@ -39,18 +41,18 @@ class MemoryBank:
         template = "--> neighbor={:3d}, accuracy={:.2f}\n"
         for _topk in topk:
             neighbor_targets = np.take(self.targets, indices[:, 1:_topk + 1], axis=0) # Exclude sample itself for eval
-            anchor_targets = np.repeat(self.targets.reshape(-1, 1), _topk, axis=1)
-            accuracy = np.mean(neighbor_targets == anchor_targets)
-            accuracy_msg += template.format(_topk, accuracy)
+            anchor_targets   = np.repeat(self.targets.reshape(-1, 1), _topk, axis=1)
+            accuracy         = np.mean(neighbor_targets == anchor_targets)
+            accuracy_msg    += template.format(_topk, accuracy)
         return accuracy_msg
 
     def update(self, features, targets, indices):
         if not isinstance(features, np.ndarray):
             features = features.detach().cpu().numpy()
         if not isinstance(targets, np.ndarray):
-            targets = targets.detach().cpu().numpy()
+            targets  = targets.detach().cpu().numpy()
         if not isinstance(indices, np.ndarray):
-            indices = indices.cpu().numpy()
+            indices  = indices.cpu().numpy()
         
         self.features[indices] = features
-        self.targets[indices] = targets
+        self.targets[indices]  = targets
